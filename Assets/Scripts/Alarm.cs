097301005f3dd0026ea519bd6037d13589c7f7df
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-06-19 13:52:27 +0300

    Update Alarm.cs

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -12,6 +12,10 @@ public class Alarm : MonoBehaviour
     private Animator _animator;
     private AudioSource _audioAlarm;
     private bool _isAlarmWork = false;
+    private float _minVolumeValue = 0f;
+    private float _maxVolumeValue = 1f;
+    private const string AlarmOn = "AlarmOn";
+    private const string AlarmOff = "AlarmOff";
 
     private void Start()
     {
@@ -26,23 +30,24 @@ public class Alarm : MonoBehaviour
             if(_isAlarmWork == false)
             {
                 _isAlarmWork = true;
-                _animator.SetTrigger("AlarmOn");
+                _animator.SetTrigger(AlarmOn);
+                
             }
             else
             {
                 _isAlarmWork = false;
-                _animator.SetTrigger("AlarmOff");
+                _animator.SetTrigger(AlarmOff);
             }            
         }
     }
 
     private void Update()
     {
-        if(_isAlarmWork == true)
+        if(_isAlarmWork == true && _audioAlarm.volume < _maxVolumeValue)
         {
             _audioAlarm.volume += _volumeStep;
         }
-        else
+        else if(_isAlarmWork == false && _audioAlarm.volume > _minVolumeValue)
         {
             _audioAlarm.volume -= _volumeStep;
         }
